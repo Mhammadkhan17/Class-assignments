bca887baacac71b5eb61de4de36afc3bc4901096
Hammad Khan <mhammadkhan740@gmail.com>
2024-12-20 18:58:27 +0500

    Update

--- assignment-06/app/page.tsx
+++ assignment-06/app/page.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 
 export default async function HomePage() {
-  
   const url = await fetch("https://jsonplaceholder.typicode.com/posts");
   const response = await url.json();
+
+  type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  };
 
   return (
     <main>
       <table className="border-collapse border border-slate-500 w-full text-left">
-        
         <thead>
           <tr>
             <th className="border border-slate-600 px-4 py-2">UserID</th>
@@ -17,18 +22,24 @@ export default async function HomePage() {
             <th className="border border-slate-600 px-4 py-2">Body</th>
           </tr>
         </thead>
-        
+
         <tbody>
-          {response.map((post: any) => (
+          {response.map((post: Post) => (
             <tr key={post.id}>
-              <td className="border border-slate-700 px-4 py-2">{post.userId}</td>
               <td className="border border-slate-700 px-4 py-2">
-                
-                <Link href={`${post.id}`} className="text-blue-600 hover:underline">
+                {post.userId}
+              </td>
+              <td className="border border-slate-700 px-4 py-2">
+                <Link
+                  href={`${post.id}`}
+                  className="text-blue-600 hover:underline"
+                >
                   {post.id}
                 </Link>
               </td>
-              <td className="border border-slate-700 px-4 py-2">{post.title}</td>
+              <td className="border border-slate-700 px-4 py-2">
+                {post.title}
+              </td>
               <td className="border border-slate-700 px-4 py-2">{post.body}</td>
             </tr>
           ))}
